eae434235e719cf04ea12812d31192caf7aa9dea
DecadeVinhLe <91219098+DecadeVinhLe@users.noreply.github.com>
2024-05-21 13:45:12 -0400

    Create Game Activity and update Main

--- app/src/main/java/vinh/le/tictactoe/MainActivity.kt
+++ app/src/main/java/vinh/le/tictactoe/MainActivity.kt
@@ -1,11 +1,30 @@
 package vinh.le.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import vinh.le.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playOfflineBtn.setOnClickListener {
+            createOfflineGame();
+
+        }
+    }
+    fun createOfflineGame(){
+        startGame();
+
+    }
+    fun startGame(){
+        startActivity(Intent(this,GameActivity::class.java));
+
     }
 }
